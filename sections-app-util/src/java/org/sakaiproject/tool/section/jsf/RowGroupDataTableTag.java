f6051ebe1126d2457df6c9f946c8e63d00e8a765
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-09 21:19:29 +0000

    SAK-15990 add ECL-2.0 copyright notice.
    git-svn-id: https://source.sakaiproject.org/svn/sections/branches/sakai_2-6-x@59924 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sections-app-util/src/java/org/sakaiproject/tool/section/jsf/RowGroupDataTableTag.java
+++ sections-app-util/src/java/org/sakaiproject/tool/section/jsf/RowGroupDataTableTag.java
@@ -1,14 +1,28 @@
+/**********************************************************************************
+ * $URL:$
+ * $Id:$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.tool.section.jsf;
-
-
 
 import org.apache.myfaces.taglib.html.ext.HtmlDataTableTag;
 
-/**
- * Author:Louis Majanja <louis@media.berkeley.edu>
- * Date: Jan 18, 2007
- * Time: 1:07:44 PM
- */
 public class RowGroupDataTableTag extends HtmlDataTableTag {
 
     public String getRendererType() {
